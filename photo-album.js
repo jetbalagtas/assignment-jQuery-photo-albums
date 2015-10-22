dd1258c055a753e96e5a5070e348aa1a0ab5e290
Jet Balagtas <jetbalagtas@gmail.com>
2015-10-22 14:39:44 -0400

    basic index & data  setup

---
+++ photo-album.js
@@ -0,0 +1,35 @@
+var sanGimignano = {
+  "photo1": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xft1/v/t1.0-9/11412282_10153370798549061_7534982708155996483_n.jpg?oh=c550c794728c9b9fa54f748a75bc2e65&oe=56BA19DA",
+  "photo2": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10898256_10153370798794061_5702472645865292205_n.jpg?oh=d7b00d1b8fd19bdc6ead5db8106d52e5&oe=56B8F33E",
+  "photo3": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xtf1/v/t1.0-9/11416211_10153370799099061_1697114344623952274_n.jpg?oh=18a21a19fa4f9708a42ddb4cc8b9dfd7&oe=568B005B"
+};
+
+var toledo = {
+  "photo1": "https://farm1.staticflickr.com/530/18318662495_a67a154e38_k.jpg",
+  "photo2": "https://farm1.staticflickr.com/559/18505329075_2537e321d0_k.jpg",
+  "photo3": "https://farm1.staticflickr.com/441/19731484683_220280a0c5_k.jpg"
+};
+
+var portofino = {
+  "photo1": "https://farm1.staticflickr.com/256/20373262746_63b39c66a1_k.jpg",
+  "photo2": "https://farm1.staticflickr.com/439/18993241476_f2cb977219_k.jpg",
+  "photo3": "https://farm1.staticflickr.com/335/19022524291_bd9f2ffbac_k.jpg"
+};
+
+var gCodeHolsters = {
+  "photo1": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/11401456_934032140005811_6226063070678326234_n.jpg?oh=35d1af1b6e359e4bebc22ea83e7a577d&oe=56CB47AD",
+  "photo2": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/11377165_934032106672481_4981159198927589582_n.jpg?oh=13a503b58741593aa67935f9a2173d44&oe=56CF266D",
+  "photo3": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11401454_934032103339148_7312047379186684496_n.jpg?oh=ac5f155a7dd9c8d884f16193bbd0e768&oe=56CE7207"
+};
+
+var mercedesAMG = {
+  "photo1": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/12119175_1051827781507968_3872939105268305955_n.jpg?oh=3baa5f10cc7b0f9ac57aaa65fcda5fb7&oe=568A7FBE",
+  "photo2": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10009752_1051827014841378_8309237809310194556_n.jpg?oh=29ed9a60e1bb65570ba4a432051c6dd2&oe=56855848",
+  "photo3": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/12115883_1051826888174724_8849946509033861154_n.jpg?oh=17dcb25e4ee55d562aa0d26c11084cbf&oe=56C1AA8F"
+};
+
+var rockwell = {
+  "photo1": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/11222586_814669065320310_7855678201071686891_n.jpg?oh=cf84f35d87bafa5c2eeb54b6de424798&oe=56895AC7",
+  "photo2": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/11811517_10153090066322756_1371702278908307536_n.jpg?oh=34fde85b9290aa9c6a346728e952b6c3&oe=56CD1432",
+  "photo3": "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11822796_815192888601261_6196001636325739165_n.jpg?oh=3482bb4ae8cbf479fd72c2598beed107&oe=5686E6F6"
+};
